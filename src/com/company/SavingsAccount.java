56c5766c48f5db71e371a0b62717066ec5379ffe
Shawn <shawncolby@billericak12.com>
2020-02-26 12:28:57 -0500

    gfhdfgdh

--- src/com/company/SavingsAccount.java
+++ src/com/company/SavingsAccount.java
@@ -2,8 +2,16 @@ package com.company;
 
 public class SavingsAccount extends Account {
     double annualInterest;
-    public SavingsAccount (int idNumber, double startBal, double annualInterest){
+    public SavingsAccount (int idNumber, double startBal, double ai){
+        super(idNumber, startBal);
+        annualInterest = ai;
+    }
+    public void withdraw (double w){
+        if (w <= currentBalance()){
+            super.decreaseBalance(w);
+        }
+    }
+    public double monthlyInterest(double i){
         
     }
-
 }
